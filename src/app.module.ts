60f0bff8ef60bc712fdb4c73311deb17049c4e5e
Anton Veselev <wh4y@mail.ru>
2022-12-13 18:42:09 +0300

    nestjs config imported as global & env file validation implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        APP_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
+})
 export class AppModule {}
