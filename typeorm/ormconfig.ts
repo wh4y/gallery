08587498ce5a992a4f766d73622dee378e65a1d2
Anton Veselev <wh4y@mail.ru>
2022-12-14 14:52:51 +0300

    ormconfig setup

---
+++ typeorm/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: configService.get<string>('DB_DIALECT'),
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: ['./src/**/*{.ts,.js}'],
+  migrations: ['./typeorm/migrations/*{.ts,.js}'],
+} as DataSourceOptions);
