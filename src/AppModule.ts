c717b32bd4db3b9879bb52ecd89a84ea82cc8281
Anton Veselev <wh4y@mail.ru>
2022-12-14 15:24:20 +0300

    typeorm module imported & setup

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +10,20 @@ import * as Joi from 'joi';
       validationSchema: Joi.object({
         APP_PORT: Joi.number().required(),
       }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get<string>('DB_DIALECT'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          logging: true,
+          migrationsRun: true,
+        } as TypeOrmModuleOptions),
     }),
   ],
 })
