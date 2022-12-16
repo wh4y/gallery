21077a1295244e45b5b12c80d0f9bf0c0cfeeb7e
Anton Veselev <wh4y@mail.ru>
2022-12-17 00:47:35 +0300

    Gallery entity relates to User as one-to-one

--- src/gallery/entity/Gallery.ts
+++ src/gallery/entity/Gallery.ts
@@ -1,5 +1,12 @@
 import { MediaFile } from './MediaFile';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../user/entity/User';
 
 @Entity()
 export class Gallery {
@@ -16,6 +23,11 @@ export class Gallery {
     cascade: ['insert'],
   })
   public mediaFiles: MediaFile[];
+
+  @OneToOne(() => User, user => user.gallery, {
+    onDelete: 'CASCADE',
+  })
+  public user: User;
 
   public static createOne(options: CreateGalleryOptions): Gallery {
     const plain = { ...options };
