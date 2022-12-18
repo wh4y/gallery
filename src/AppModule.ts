5d6c9f47c916d4004e5a7f97ba5531a98699094c
Anton Veselev <wh4y@mail.ru>
2022-12-18 15:19:29 +0300

    AuthModule added

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -4,6 +4,7 @@ import * as Joi from 'joi';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { GalleryModule } from './gallery/GalleryModule';
 import { join } from 'path';
+import { AuthModule } from './auth/AuthModule';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { join } from 'path';
         } as TypeOrmModuleOptions),
     }),
     GalleryModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/auth/AuthModule.ts
+++ src/auth/AuthModule.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../user/UserModule';
+import { AuthController } from './controller/AuthController';
+import { AuthService } from './service/AuthService';
 
 @Module({
   imports: [UserModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
