5945f03668ccd4352761dce5bdb31afb998932f9
Anton Veselev <wh4y@mail.ru>
2022-12-20 23:31:45 +0300

    server static assets configured

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
   const PORT = configService.get<number>('APP_PORT');
 
   app.use(cookieParser());
+  app.useStaticAssets(join(__dirname, '..', '..', 'upload'), {
+    index: false,
+    prefix: '/gallery/',
+  });
 
   await app.listen(PORT as number);
 }
