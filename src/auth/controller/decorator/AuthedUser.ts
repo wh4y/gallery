788143ff6bfc8a5d3c23d47d34e9d0eb68f5b9b1
Anton Veselev <wh4y@mail.ru>
2022-12-20 22:26:08 +0300

    AuthedUser decorator implemented

---
+++ src/auth/controller/decorator/AuthedUser.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../../../user/entity/User';
+
+export const AuthedUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): User => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user as User;
+  },
+);
