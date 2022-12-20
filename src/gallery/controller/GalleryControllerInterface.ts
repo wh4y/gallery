7b35c8b718f0a63fbf1b7e46993f0276b9620db0
Anton Veselev <wh4y@mail.ru>
2022-12-20 22:50:24 +0300

    addVideoToGallery, addImageToGallery added

--- src/gallery/controller/GalleryControllerInterface.ts
+++ src/gallery/controller/GalleryControllerInterface.ts
@@ -1,11 +1,14 @@
 import { Gallery } from '../entity/Gallery';
-import { MediaFile } from '../entity/MediaFile';
 import { Response } from 'express';
+import { User } from '../../user/entity/User';
 
 export interface GalleryControllerInterface {
   getGalleryById(id: number): Promise<Gallery>;
-  addFileToGalleryWithId(id: number, file: unknown): Promise<void>;
-  addFileToGalleryWithId(id: number, mediaFile: MediaFile): Promise<void>;
+
+  addVideoToGallery(user: User, file: Express.Multer.File): Promise<void>;
+
+  addImageToGallery(user: User, file: Express.Multer.File): Promise<void>;
+
   downloadFileById(
     galleryId: number,
     fileId: number,
