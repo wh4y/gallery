5c1a36dbc78c32211069d4a6009ca667510bef58
Anton Veselev <wh4y@mail.ru>
2022-12-24 15:38:48 +0300

    FileBlockedUserListRepo implemented

---
+++ src/gallery/repository/FileBlockedUserListRepo.ts
@@ -0,0 +1,4 @@
+import { Repository } from 'typeorm';
+import { FileBlockedUserList } from '../entity/FileBlockedUserList';
+
+export class FileBlockedUserListRepo extends Repository<FileBlockedUserList> {}
